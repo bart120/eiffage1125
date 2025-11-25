ff8ffdb94b9882b7b2d21c965fc9f768dab5881f
Vincent Leclerc <vleclerc@inow.fr>
2025-11-25 12:39:42 +0100

    template

--- formation/src/app/pages/authentication/login/login.component.ts
+++ formation/src/app/pages/authentication/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  @ViewChild('pass')
+  //password: ElementRef | undefined;
+  password!: ElementRef;
+
+  /*onLogin(username: HTMLInputElement): void {
+    console.log('Login attempt for user:', username.value);
+  }*/
+
+  constructor() {
+
+  }
+
+  onLogin(username: string): void {
+    console.log(`Login attempt for user: ${username} with password: ${this.password.nativeElement.value}`);
+  }
 }
